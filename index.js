524130597359c87e5c912bbe126d2d64ceaeb669
webdevelopermohiuddin <mohiuddinmazumder94@gmail.com>
2020-09-26 13:49:19 +0600

    install express and nodemon

--- index.js
+++ index.js
@@ -3,9 +3,28 @@ const express = require('express');
 
 const app = express();
 
+const users = ["asad", "hablu", "sabana", "josim", "kabila", "hakim"]
 
 app.get('/', (req, res) =>{
-    res.send('I know how to open node.. YAY')
+    const fruit = {
+        product: 'ada',
+        price: '230'
+    }
+    res.send(fruit);
+})
+
+app.get('/fruits/banana', (req, res) => {
+    const product = {
+        fruit: 'banana', quantity: 10000, price: 100000
+    }
+    res.send(product);
+})
+ 
+app.get('/users/:id', (req, res) => {
+    const id = req.params.id;
+    console.log(req.query);
+    const name = users[id];
+    res.send({id, name});
 })
 
 app.listen(4000, () => console.log('listening to port 4000'));
